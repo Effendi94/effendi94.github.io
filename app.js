1c0f8dbda50fa911c192fdebfa439370a97d5d04
Effendi Nugraha <66555086+Effendi94@users.noreply.github.com>
2022-08-05 08:30:57 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -8,12 +8,9 @@ async function closeWebView(base64) {
     const data = [
         {
             type: "share",
-            data: base64,
+            base64: base64,
         },
     ];
-    // Print.postMessage(data);
-
-    console.log("### JS RUN ###");
     if (isFlutterInAppWebViewReady) {
         window.flutter_inappwebview.callHandler("shareHandler", ...data);
     }
